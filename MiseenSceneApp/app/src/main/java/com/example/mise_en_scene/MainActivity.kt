693122d52ae8c94a0c5f907958689cb1947e5562
Modilon <mateusgaspar81@gmail.com>
2020-10-01 03:49:29 -0300

    Kotlin Screens

---
+++ MiseenSceneApp/app/src/main/java/com/example/mise_en_scene/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mise_en_scene
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
